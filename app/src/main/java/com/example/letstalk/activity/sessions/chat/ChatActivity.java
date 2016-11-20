269d2967be152722091ae12793b5db0c8be32ced
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-11-20 22:34:13 +0200

    creating camera class and take photo functionality

--- app/src/main/java/com/example/letstalk/activity/sessions/chat/ChatActivity.java
+++ app/src/main/java/com/example/letstalk/activity/sessions/chat/ChatActivity.java
@@ -1,10 +1,7 @@
 package com.example.letstalk.activity.sessions.chat;
 
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import com.example.letstalk.Utilities.Camera;
 import com.example.letstalk.configuration.Config;
 import com.example.letstalk.R;
 import com.example.letstalk.domain.message.ChatMessage;
@@ -25,17 +23,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import static android.view.View.*;
 
-/**
- * Created by teodo on 25/09/2016.
- */
 public class ChatActivity extends AppCompatActivity implements OnClickListener{
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private RelativeLayout chatHolderRelativeLayout;
     private ListView listView;
@@ -47,101 +41,30 @@ public class ChatActivity extends AppCompatActivity implements OnClickListener{
     private ChatArrayAdapter chatArrayAdapter;
     private DatabaseReference databaseReference;
     private String user;
-
-    private RelativeLayout getChatHolderRelativeLayout() {
-        return this.chatHolderRelativeLayout;
-    }
-
-    private void setChatHolderRelativeLayout(RelativeLayout chatHolderRelativeLayout) {
-        this.chatHolderRelativeLayout = chatHolderRelativeLayout;
-    }
-
-    private ListView getListView() {
-        return this.listView;
-    }
-
-    private void setListView(ListView listView) {
-        this.listView = listView;
-    }
-
-    private EditText getEditMessageText() {
-        return this.editMessageText;
-    }
-
-    private void setEditMessageText(EditText editMessageText) {
-        this.editMessageText = editMessageText;
-    }
-
-    private Button getSendMessageButton() {
-        return this.sendMessageButton;
-    }
-
-    private void setSendMessageButton(Button sendMessageButton) {
-        this.sendMessageButton = sendMessageButton;
-    }
-    public Button getGeolocationButton() {
-        return geolocationButton;
-    }
-
-    public void setGeolocationButton(Button geolocationButton) {
-        this.geolocationButton = geolocationButton;
-    }
-
-    public Button getCameraButton() {
-        return cameraButton;
-    }
-
-    public void setCameraButton(Button cameraButton) {
-        this.cameraButton = cameraButton;
-    }
-
-    public ImageView getPicture() {
-        return picture;
-    }
-
-    public void setPicture(ImageView picture) {
-        this.picture = picture;
-    }
-
-
-    private ChatArrayAdapter getChatArrayAdapter() {
-        return this.chatArrayAdapter;
-    }
-
-    private void setChatArrayAdapter(ChatArrayAdapter chatArrayAdapter) {
-        this.chatArrayAdapter = chatArrayAdapter;
-    }
-
-    private DatabaseReference getDatabaseReference() {
-        return this.databaseReference;
-    }
-
-    private void setDatabaseReference(DatabaseReference databaseReference) {
-        this.databaseReference = databaseReference;
-    }
+    private Camera camera;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_holder);
 
-        this.setDatabaseReference(FirebaseDatabase.getInstance().getReference().child(Config.CHILD_CHATS).child("advisor_user"));
+        this.databaseReference = (FirebaseDatabase.getInstance().getReference().child(Config.CHILD_CHATS).child("advisor_user"));
 
-        this.setListView((ListView)findViewById(R.id.listViewMessageHolder));
-        this.setEditMessageText( (EditText)findViewById(R.id.messageText));
-        this.setSendMessageButton((Button)findViewById(R.id.buttonSendMessage));
-        this.setCameraButton((Button)findViewById(R.id.btn_camera));
-        this.setGeolocationButton((Button)findViewById(R.id.btn_geolocation));
-        this.setChatArrayAdapter(new ChatArrayAdapter(this, R.layout.chat_message));
-        this.setPicture((ImageView)findViewById(R.id.imageView_picture));
+        this.listView = ((ListView)findViewById(R.id.listViewMessageHolder));
+        this.editMessageText = ( (EditText)findViewById(R.id.messageText));
+        this.sendMessageButton = ((Button)findViewById(R.id.buttonSendMessage));
+        this.cameraButton = ((Button)findViewById(R.id.btn_camera));
+        this.geolocationButton = ((Button)findViewById(R.id.btn_geolocation));
+        this.chatArrayAdapter = (new ChatArrayAdapter(this, R.layout.chat_message));
+        this.picture = ((ImageView)findViewById(R.id.imageView_picture));
 
-        this.getSendMessageButton().setOnClickListener(this);
-        this.getCameraButton().setOnClickListener(this);
-        this.getGeolocationButton().setOnClickListener(this);
+        this.sendMessageButton.setOnClickListener(this);
+        this.cameraButton.setOnClickListener(this);
+        this.geolocationButton.setOnClickListener(this);
 
-        this.getListView().setAdapter(this.getChatArrayAdapter());
+        this.listView.setAdapter(this.chatArrayAdapter);
 
-        this.getDatabaseReference().addChildEventListener(new ChildEventListener() {
+        this.databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 appendMessage(dataSnapshot);
@@ -166,10 +89,8 @@ public class ChatActivity extends AppCompatActivity implements OnClickListener{
             public void onCancelled(DatabaseError databaseError) {
 
             }
-
         });
     }
-
 
     @Override
     public void onClick(View v) {
@@ -179,7 +100,7 @@ public class ChatActivity extends AppCompatActivity implements OnClickListener{
                 eraseText();
                 break;
             case R.id.btn_camera:
-                dispatchTakePictureIntent();
+                File photo = this.camera.dispatchTakePictureIntent(this);
             break;
             case R.id.btn_geolocation:
 
@@ -210,20 +131,8 @@ public class ChatActivity extends AppCompatActivity implements OnClickListener{
         ((BaseAdapter)this.listView.getAdapter()).notifyDataSetChanged();
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            this.getPicture().setImageBitmap(imageBitmap);
-        }
-    }
+
+
 
 }
