f1eb6f62e1582bdf0577de0a33b8c92e6a25a56f
Veronika Dzhambazova <veronika.dzhambazova@abv.bg>
2016-12-23 13:25:37 +0200

    final

--- app/src/main/java/com/example/letstalk/activity/sessions/SessionTalkFragment.java
+++ app/src/main/java/com/example/letstalk/activity/sessions/SessionTalkFragment.java
@@ -2,12 +2,10 @@ package com.example.letstalk.activity.sessions;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/example/letstalk/activity/sessions/SessionsActivity.java
+++ app/src/main/java/com/example/letstalk/activity/sessions/SessionsActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import com.example.letstalk.R;
